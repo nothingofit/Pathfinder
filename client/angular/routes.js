179e578b0144a22d63c625fdd2ad6cd70e693985
David Jimenez <davidjosuedjdj@gmail.com>
2015-10-24 22:27:17 -0700

    Added route for login success

--- client/angular/routes.js
+++ client/angular/routes.js
@@ -22,6 +22,9 @@ app.config( function ($routeProvider) {
     .when('/loginSuccess', {
       templateUrl: '/partials/success.html'
     })
+    .when('/loginSuccess', {
+      templateUrl: '/partials/success.html'
+    })
     .otherwise({
       redirectTo: '/'
     });
