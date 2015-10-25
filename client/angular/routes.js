c87195eb8212dcf866a7379e9f273ddbe54b9864
Hana Um <hanau12@gmail.com>
2015-10-25 01:02:55 -0700

    Created skill profile page.

--- client/angular/routes.js
+++ client/angular/routes.js
@@ -19,6 +19,9 @@ app.config( function ($routeProvider) {
     .when('/jobprofile', {
       templateUrl: '/angular/job_profile/jobprofile.html'
     })
+    .when('/skillprofile', {
+      templateUrl: '/angular/skill_profile/skillprofile.html'
+    })
     .when('/loginSuccess', {
       templateUrl: '/partials/success.html'
     })
