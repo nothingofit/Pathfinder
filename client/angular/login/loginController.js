cade4ff6591fd9d27068e662daeb738e0da2ded5
David Jimenez <davidjosuedjdj@gmail.com>
2015-10-25 03:41:08 -0700

    Worked on login

--- client/angular/login/loginController.js
+++ client/angular/login/loginController.js
@@ -1,7 +1,32 @@
-app.controller('loginController', function($scope){
+app.controller('loginController', function($scope, loginFactory){
 
 	$scope.loginClick = function(){
-		console.log("inside the controller");
+		// loginFactory.findUser($scope.login, function(data){
+		// 	console.log(data);
+		// })
+		$.ajax({
+			url       : 'https://api-us.clusterpoint.com/v4/102323/hackingEDU/_query',
+			type      : 'POST',
+			dataType  : 'json',
+			data      : 'SELECT * FROM hackingEDU WHERE "username" == ' + $scope.login.username,
+			beforeSend: function (xhr) {
+				xhr.setRequestHeader('Authorization', 'Basic ' + btoa('davidjosuejimenez@hotmail.com:codingDojoN'));
+			},
+			success   : function (data) {
+				if (typeof success != 'undefined') {
+					success(data);
+					console.log(data);
+				}
+			},
+			fail      : function (data) {
+				alert(data.error);
+				if (typeof fail != 'undefined') {
+					fail(data);
+				}
+			}
+		}).done(function(data){
+			console.log(data);
+		});
 	}
 
 })
